2c577dc63c982148576d8b261b0844070fce8461
ogurchik228yt <149517275+ogurchik228yt@users.noreply.github.com>
2024-05-09 20:32:12 +0300

    Add files via upload

---
+++ kb-backend-week-2-startkit-main/controllers/vote.js
@@ -0,0 +1,106 @@
+const fs = require("fs").promises;
+const parseBody = require("../appModules/http-utils/parse-body");
+const { config, createRating, updateRating } = require("../appModules/rating");
+const httpUtils = require ("../appModules/http-utils")
+
+async function voteRouteController(req, res) {
+  if (req.method !== "POST") {
+    res.statusCode = 404;
+    res.end("Not Found");
+  } else {
+      res.statusCode = 200;
+      const body = await httpUtils.parseBody(req);
+      const data = JSON.parse(body);
+
+      const rating = createRating(data, config.WEIGHT);
+
+      const ratingFile = await fs.readFile(config.PATH_TO_RATING_FILE);
+      const ratingArray = JSON.parse(ratingFile);
+      const newRating = updateRating(ratingArray, data.id, rating);
+
+      await fs.writeFile(config.PATH_TO_RATING_FILE, JSON.stringify(newRating));
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(newRating.sort((a, b) => b.rating - a.rating)));
+    }
+  }
+
+module.exports = voteRouteController; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**const httpUtils = require("../appModules/http-utils");
+const { createRating, updateRating } = require("../appModules/rating");
+const { PATH_TO_RATING_FILE, WEIGHT } = require("../appModules/rating/config");
+const {fs} = require('fs');
+const { config } = require("../appModules/rating")
+
+async function voteRouteController(req, res) {
+    if (req.method !== "POST") {
+        res.statusCode = 404;
+        res.end("Not Found");
+    } else {
+        res.statusCode = 200;
+        const body = await httpUtils.parseBody(req);
+        const data = JSON.parse(body);
+        const rating = createRating(data, WEIGHT);
+
+        const ratingFile = await fs.readFile(config, PATH_TO_RATING_FILE);
+        const ratingArray = JSON.parse(ratingFile);
+        const newRating = updateRating(ratingArray, data.id, rating)
+
+        await fs.writeFile(config, PATH_TO_RATING_FILE, JSON.stringify(newRating));
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify(newRating.sort((a,b) => b.rating - a.rating)))
+    }
+}
+
+module.exports = voteRouteController;
+**/
+
+
+/**const httpUtils = require("../appModules/http-utils");
+const { createRating, updateRating } = require("../appModules/rating");
+const { PATH_TO_RATING_FILE, WEIGHT } = require("../appModules/rating/config");
+const {fs} = require('fs');
+
+async function voteRouteController(req, res) {
+    if (req.method !== "POST") {
+        res.statusCode = 404;
+        res.end("Not Found");
+    } else {
+        res.statusCode = 200;
+        const body = await httpUtils.parseBody(req);
+        const data = JSON.parse(body);
+        const rating = createRating(data, config.WEIGHT);
+
+        const ratingFile = await fs.readFile(config, PATH_TO_RATING_FILE);
+        const ratingArray = JSON.parse(ratingFile);
+        const newRating = updateRating(ratingArray, data.id, rating)
+
+        await fs.writeFile(config, PATH_TO_RATING_FILE, JSON.stringify(newRating));
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify(newRating.sort((a,b) => b.rating - a.rating)))
+    }
+}
+
+module.exports = voteRouteController;
+**/
